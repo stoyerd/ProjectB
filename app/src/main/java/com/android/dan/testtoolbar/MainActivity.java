3be36e5ebc2e153c7e0bc60536f51fbab2e3457c
Dan <stoyerd@oregonstate.edu>
2015-11-30 20:32:40 -0800

    Test commit
    Changes to
    MainActivity (comment)
    strings.xml (changed name in the app)

--- app/src/main/java/com/android/dan/testtoolbar/MainActivity.java
+++ app/src/main/java/com/android/dan/testtoolbar/MainActivity.java
@@ -71,7 +71,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            }
 //        });
     }
-// dloughlin test
+// dloughlin tes
+    // Dan Stoyer comment
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
