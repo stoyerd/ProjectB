15778cb2d476e8a1383ae4e12334f25bfab87598
jsdiddon <justin@delaris.com>
2015-12-02 05:47:16 -0800

    Basic request being made to UPC database API and response received. Added code to remove API request from queue if app is closed.

--- app/src/main/java/com/android/dan/testtoolbar/ManualUpcSearch.java
+++ app/src/main/java/com/android/dan/testtoolbar/ManualUpcSearch.java
@@ -1,5 +1,6 @@
 package com.android.dan.testtoolbar;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,11 +10,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.w3c.dom.Text;
 
 public class ManualUpcSearch extends AppCompatActivity {
 
-    private TextView textView;
+    TextView textView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    RequestQueue queue;             // Where requests go to be made.
+    StringRequest stringRequest;    // Actual request.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +40,10 @@ public class ManualUpcSearch extends AppCompatActivity {
         setContentView(R.layout.activity_manual_upc_search);
         textView = (TextView) findViewById(R.id.action_manual_upc);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        queue = Volley.newRequestQueue(this);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -33,12 +54,41 @@ public class ManualUpcSearch extends AppCompatActivity {
 //            }
 //        });
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /* Manual UPC Lookup home. */
     public void upcLookup(View view) {
-        Log.d("Render", "temp");
-        textView.setText("test");
+        String url = getString(R.string.upc_lookup_uri);    // Get API url.
+        Log.d("API URL", url);
+
+        // Make request
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+                public void onResponse(String response) {
+                    Log.d("Response", response.substring(0, 500));
+                }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR!", "Error doing something");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);           // This actually sends the request.
     }
+
+    /* Override onStop() */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (queue != null) {
+            stringRequest.cancel();
+        }
+    }
+
 
 }
