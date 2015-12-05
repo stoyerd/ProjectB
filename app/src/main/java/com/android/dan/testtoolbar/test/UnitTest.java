e6bd8feb8cfaa0f0225869df5db5e2a401a2ac1e
loughlid <loughlid@onid.orst.edu>
2015-12-05 14:52:48 -0700

    Added unit test for zipcode

--- app/src/main/java/com/android/dan/testtoolbar/test/UnitTest.java
+++ app/src/main/java/com/android/dan/testtoolbar/test/UnitTest.java
@@ -2,15 +2,40 @@ package com.android.dan.testtoolbar.test;
 
 import android.test.InstrumentationTestCase;
 
+import com.android.dan.testtoolbar.zipcode.Zipcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 /**
  * Created by Dan on 12/5/2015.
  */
 public class UnitTest extends InstrumentationTestCase{
 
-    public void test() throws Exception {
+    public void testZipcodeObjectCreate() throws Exception {
+        // Create a zipcode, with city and items
+        List<String> plasticItems = new ArrayList<>();
+        plasticItems.add("plastic cups");
+        plasticItems.add("plastic utensils");
 
-        assertFalse(4 == 5+2);
-        assertTrue(4 == 2+2);
+        List<String> glassItems = new ArrayList<>();
+        glassItems.add("beer bottles");
+        glassItems.add("coke bottles");
+
+        Map<String, List<String>> items = new HashMap<>();
+        items.put("plastic", plasticItems);
+        items.put("glass", glassItems);
+
+        Zipcode zip = new Zipcode(10011, "New York", items);
+
+        assertTrue(10011 == zip.getID());
+        assertTrue("New York" == zip.getCity());
+
+        zip.getStandards().get("glass");
+        assertTrue(glassItems == zip.getStandards().get("glass"))
 
     }
 }
