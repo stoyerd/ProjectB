5aaa3b67bd98cb401c3ec8d2af1cb8387f39e73d
mu5tan6 <crg.chapman@gmail.com>
2015-12-06 09:16:02 -0800

    Adding barcode scanner function to mainActivity and related files

--- app/src/main/java/com/android/dan/location/MainActivity.java
+++ app/src/main/java/com/android/dan/location/MainActivity.java
@@ -8,12 +8,22 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.widget.AdapterView;
+import android.util.SparseArray;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import com.android.dan.location.objects.Zipcode;
 
@@ -117,7 +127,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         product2 = (EditText) findViewById(R.id.product_edit2);
         product3 = (EditText) findViewById(R.id.product_edit3);
 
+		Button scanBtn = (Button) findViewById(R.id.scanButton);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView myTextView = (TextView) findViewById(R.id.txtContent);
+                ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.puppy);
 
+                myImageView.setImageBitmap(myBitmap);
+
+
+                myTextView.setText(getValueOfBarCode(myBitmap));
+            }
+        });
     }
 
     @Override
@@ -269,5 +294,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return true;
+    }
+	
+	/**
+	* Takes a bitmap as input and returns a 
+	* string representing the value of the barcode
+	*/
+	protected String getValueOfBarCode(Bitmap bitmapInput)  {
+        TextView myTextView = (TextView) findViewById(R.id.txtContent);
+        BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+
+        if (!detector.isOperational()) {
+            myTextView.setText("Could not set up the detector");
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(bitmapInput).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+
+        return thisCode.rawValue;
     }
 }
